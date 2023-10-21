f1ae795f2443751fd39c20befa6e743963deb1bc
sarah-e-c <71732260+sarah-e-c@users.noreply.github.com>
2023-10-21 15:16:17 -0400

    added spider enemy and enemies (around), updated text box font for labels

--- src/Effects/enemy_death_effect.gd
+++ src/Effects/enemy_death_effect.gd
@@ -2,10 +2,13 @@ extends Node2D
 
 @onready var animatedSprite = $AnimatedSprite2D
 
+var flip_h = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	animatedSprite.frame = 0
 	animatedSprite.play()
+	animatedSprite.flip_h = flip_h
 
 	
 func _on_animated_sprite_2d_animation_finished():
--- src/Enemies/spider-alien.gd
+++ src/Enemies/spider-alien.gd
@@ -1,32 +1,45 @@
 extends CharacterBody2D
 
 var knockback = Vector2.ZERO
-@export var health = 5
+var health = 3
 var state = IDLE
-var MAX_SPEED = 70
+var MAX_SPEED = 30
+
+var CHARGE_SPEED = MAX_SPEED * 5
+const CHARGE_TIME = 1
 
 const ACCELLERATION = 200
-
-@onready var world = $/root/World
 
 
 enum {
 	IDLE,
 	WANDER,
-	CHASE
+	CHASE,
+	PREPARE,
+	CHARGE,
+	COOLDOWN
 }
-
+@onready var world = $/root/World
 @onready var hurtbox = $Hurtbox
 @onready var wanderController = $WanderController
 @onready var  detectionZone = $PlayerDetectionZone
 @onready var sprite = $animatedBat
 @onready var softCollision = $SoftCollision
 const EnemyDeathEffect = preload("res://Effects/enemy_death_effect.tscn")
+const SpiderDeathEffect = preload("res://Effects/spider_death_effect.tscn")
 @onready var visibilityController = $"Enemy Visibility Controller"
 
 @onready var hitbox = $Hitbox
 
+@onready var chargeController = $ChargeController
+@onready var chargeZone = $ChargeZone
+@onready var chargeParticles = $ChargeParticles
+
+func _ready():
+	sprite.play("default")
+
 func _physics_process(delta):
+#	print(state)
 	match state:
 		IDLE:
 			knockback = knockback.move_toward(Vector2.ZERO, 200 * delta)
@@ -39,6 +52,12 @@ func _physics_process(delta):
 				wanderController.set_wander_timer(randf_range(1, 3))
 			if detectionZone.player_is_detected():
 				state = CHASE
+			
+			if chargeZone.player_is_detected():
+				state = PREPARE
+				chargeController.start_preparing(chargeZone.player)
+				chargeParticles.emitting = true
+				sprite.speed_scale = 4
 		WANDER: 
 			if wanderController.get_time_left() <= .1:
 				state = pick_random_state([IDLE, WANDER])
@@ -50,23 +69,49 @@ func _physics_process(delta):
 				wanderController.set_wander_timer(randf_range(1,2))
 			
 			if detectionZone.player_is_detected():
-				state = CHASE 
+				state = CHASE
+			
+			if chargeZone.player_is_detected():
+				state = PREPARE
+				chargeController.start_preparing(chargeZone.player)
+				chargeParticles.emitting = true
+				sprite.speed_scale = 4
 		CHASE:
 			var player = detectionZone.player
 			if player != null:
 				accellerate_towards(player.global_position, delta)
 			if not detectionZone.player_is_detected():
 				state = IDLE
+			if chargeZone.player_is_detected():
+				state = PREPARE
+				chargeController.start_preparing(chargeZone.player)
+				chargeParticles.emitting = true
+				sprite.speed_scale = 4
 			sprite.flip_h = velocity.x < 0
-			
-			
+		PREPARE:
+			chargeController.prepare()
+			velocity = velocity.move_toward(Vector2.ZERO, 400 * delta)
+			sprite.flip_h = chargeController.targetVector.x < 0
+		CHARGE:
+			sprite.flip_h = velocity.x < 0
+#			if get_slide_collision_count() > 0:
+#				print("collided with wall - set cooldown")
+#				state = COOLDOWN
+#				hitbox.knockback_strength = 100
+#				chargeController.dashing = false
+		COOLDOWN:
+			velocity = velocity.move_toward(Vector2.ZERO, 400 * delta)
 	if softCollision.is_colliding():
 		velocity += softCollision.get_push_vector() * delta * 400
 	
 	if health <= 0:
 		world.enemy_died(false)
 		var enemyDeathEffect = EnemyDeathEffect.instantiate()
+		var spiderDeathEffect = SpiderDeathEffect.instantiate()
+		get_parent().add_child(spiderDeathEffect)
 		get_parent().add_child(enemyDeathEffect)
+		spiderDeathEffect.global_position = global_position
+		spiderDeathEffect.flip_h = sprite.flip_h
 		enemyDeathEffect.global_position = global_position
 		queue_free()
 	
@@ -94,3 +139,24 @@ func pick_random_state(states):
 	states.shuffle()
 	return states.pop_front()
 	
+
+
+func _on_charge_controller_charge_prep_done(targetVector):
+	state = CHARGE
+	velocity = targetVector * CHARGE_SPEED
+	sprite.play("jump_animation")
+	hitbox.knockback_strength = 300
+	chargeParticles.emitting = false
+	sprite.speed_scale = 1
+
+
+func _on_charge_controller_charge_done():
+	sprite.play("default")
+	if state == CHARGE:
+#		print("charge timer done - set cooldown")
+		state = COOLDOWN
+		hitbox.knockback_strength = 100
+
+
+func _on_charge_controller_cooldown_done():
+	state = IDLE
