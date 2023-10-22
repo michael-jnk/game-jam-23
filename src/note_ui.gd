8dc97250d80d8dd7f36fb52989bf7e37516164ca
sarah-e-c <71732260+sarah-e-c@users.noreply.github.com>
2023-10-22 18:41:17 -0400

    fixed merge conflict

--- src/note_ui.gd
+++ src/note_ui.gd
@@ -3,7 +3,8 @@ extends Control
 @onready var artifact1 = $HSplitContainer/Artifact1
 @onready var artifact2 = $HSplitContainer/HSplitContainer/Artifact2
 @onready var artifact3 = $HSplitContainer/HSplitContainer/HSplitContainer2/Artifact3
-@onready var artifact4 = $HSplitContainer/HSplitContainer/HSplitContainer2/Artifact4
+@onready var artifact4 = $HSplitContainer/HSplitContainer/HSplitContainer2/HSplitContainer3/Artifact4
+@onready var artifact5 = $HSplitContainer/HSplitContainer/HSplitContainer2/HSplitContainer3/Artifact5
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,6 +12,7 @@ func _ready():
 	artifact2.visible = false
 	artifact3.visible = false
 	artifact4.visible = false
+	artifact5.visible = false
 
 
 func display_artifact(artifactNum):
@@ -24,3 +26,5 @@ func display_artifact(artifactNum):
 			artifact3.visible = true
 		3:
 			artifact4.visible = true
+		4:
+			artifact5.visible = true
