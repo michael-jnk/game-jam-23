67a306b973572cbd33b34f44f2f9b9071363d781
sarah-e-c <71732260+sarah-e-c@users.noreply.github.com>
2023-10-22 19:57:48 -0400

    artifact text:

--- src/UI/ArtifactController.gd
+++ src/UI/ArtifactController.gd
@@ -8,12 +8,35 @@ var artifacts_found = 0
 signal artifact_condition_met
 
 
-const artifact_texts = [["This book belonged to Pilot 93-D, a capable and kind navigator on the Aeternitas 1 crew.",
+const artifact_texts = [
+
+# artifact 1	
+["This book belonged to Pilot 93-D, a capable and kind navigator on the Aeternitas 1 crew.",
 "Unassuming to those who didn’t know them, 93-D was actually a great chef and an avid bookworm.",
- "This specific book, called Nol Skrulgad and the Seven Swords,", "was the 4th installment of the Auvrul Chronicles series, an all time favorite of theirs."],
- ["asdlkfj;as;ldkfja;lskdfj"], 
-["asdlf;kjasd;lfjk"],
-["asdfl;kjasdf;lkjadsfa"]]
+ "This specific book, called Nol Skrulgad and the Seven Swords,",
+ "was the 4th installment of the Auvrul Chronicles series, an all time favorite of theirs.",
+"It is odd that it is laying around in the entranceway."],
+
+#artifact 2
+ ["This necklace belonged to Mission Specialist 872-L,",
+ "a methodical classically trained pianist turned aeronautical scientist.",
+ "It was a family heirloom passed down in their family for generations,",
+ "and said to bring good fortune, having been originally purchased by an ancestor from the Silk Road in 140 BCE.",
+ "It is inlaid with garnets and rubies and would fetch an astronomically high price if sold."],
+
+# artifact 3 
+["This Skibur Cube belonged to Commander 34-A who was the leader of Aeternitas 1.",
+ "Back in their heyday, they were a 5-time speedcubing world champion.",
+ "Praised as a courageous and charismatic leader,",
+ "Commander 34-A was dearly missed by family and friends and the ASAN community on Earth"],
+
+# artifact 4
+["This key belonged to Flight Engineer 339-T,",
+ "known for their invention of the Robotized Warfare Manipulsator Matrix",
+ "and Adaptable Probe Transferator Device.",
+ "She was rumored to open their secret vault in the Swiss Alps with all of their less savory technologies.",
+ "As an astronaut, 339-T was the most sociable and amiable of the crew,",
+ "and enjoyed watching reality TV shows in their free time."]]
 
 func _ready():
 	artifactTextBox.visible = false
