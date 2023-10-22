7a5bd2ad045e798356640cb3e58e847a8b050651
sarah-e-c <71732260+sarah-e-c@users.noreply.github.com>
2023-10-22 14:49:41 -0400

    edited the hazard tiles to be more sparse (we don't need the color and i got feedback ghat they were confusing)

--- src/World/ship_map.gd
+++ src/World/ship_map.gd
@@ -2,11 +2,14 @@ extends Node2D
 
 
 @onready var flooring = $Flooring
+@onready var map = $Map
 # Called when the node enters the scene tree for the first time
 
 func world(isWorld1):
 	if isWorld1:
 		flooring.material.set_shader_parameter("is_past", true)
+		map.material.set_shader_parameter("is_past", true)
 	else:
 		flooring.material.set_shader_parameter("is_past", false)
+		map.material.set_shader_parameter("is_past", false)
 	
